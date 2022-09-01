897604bea89023766b1b9bb1f2bd54e3b1edd4e1
deljanin <pecadeljanin@gmail.com>
2022-09-02 00:57:29 +0200

    Fixed about me bg & centered text

--- src/componentes/about_me_section/AboutMe.jsx
+++ src/componentes/about_me_section/AboutMe.jsx
@@ -39,38 +39,41 @@ function AboutMe() {
         <div></div>
       </div>
       <div className="about__lower"></div>
-      <div className="about__title_wrapper" ref={aboutTitle}>
-        <h1 className="about__title">About</h1>
-        <h1 className="about__anim">
-          <Typewriter
-            options={{
-              strings: ["Me", "Code", "Art"],
-              autoStart: true,
-              loop: true,
-              cursor: "_",
-            }}
-          />
-        </h1>
-      </div>
-      <div className="about__text" ref={textBlock}>
-        <p>
-          Hi, I am{" "}
-          <span className="general__text_highlight">Petar Deljanin</span>, a
-          Serbian software engineer currently based in Slovenia (Europe). I am
-          finishing my undergraduate computer science studies at{" "}
-          <span className="general__text_highlight">
-            <a href="https://www.famnit.upr.si/en/">UP FAMNIT. </a>
-          </span>
-          So I am looking for a job in Singapore because I want to "start a new
-          life" there, because I think it is a city of future and innovation and
-          because I want to be a part of a team in a world-class software
-          engineering company. <br />I enjoy writing code and learning new
-          technologies daily, as I am very passionate about this field.
-          <br />
-          In my free time I love to listen to intellectual podcasts, to create
-          art through drawing and writing, and to learn by reading books and by
-          taking online courses.
-        </p>
+      <div className="about__content_wrapper">
+        <div className="about__title_wrapper" ref={aboutTitle}>
+          <h1 className="about__title">About</h1>
+          <h1 className="about__anim">
+            <Typewriter
+              options={{
+                strings: ["Me", "Code", "Art"],
+                autoStart: true,
+                loop: true,
+                cursor: "_",
+              }}
+            />
+          </h1>
+        </div>
+        <div className="about__text" ref={textBlock}>
+          <p>
+            Hi, I am{" "}
+            <span className="general__text_highlight">Petar Deljanin</span>, a
+            Serbian software engineer currently based in Slovenia (Europe). I am
+            finishing my undergraduate computer science studies at{" "}
+            <span className="general__text_highlight">
+              <a href="https://www.famnit.upr.si/en/">UP FAMNIT. </a>
+            </span>
+            So I am looking for a job in Singapore because I want to "start a
+            new life" there, because I think it is a city of future and
+            innovation and because I want to be a part of a team in a
+            world-class software engineering company. <br />I enjoy writing code
+            and learning new technologies daily, as I am very passionate about
+            this field.
+            <br />
+            In my free time I love to listen to intellectual podcasts, to create
+            art through drawing and writing, and to learn by reading books and
+            by taking online courses.
+          </p>
+        </div>
       </div>
     </div>
   );
